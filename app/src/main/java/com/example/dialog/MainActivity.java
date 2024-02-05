080b702cfb947936ea88e41812bb5cd96fbbd1cc
forestdesu <aa2652839@gmail.com>
2024-02-05 13:37:12 +0900

    Initial commit

---
+++ app/src/main/java/com/example/dialog/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.dialog;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.View;
+import android.widget.*;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button bgButton;
+    public ConstraintLayout constraintLayout;
+    Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bgButton = (Button)findViewById(R.id.button_fon);
+        constraintLayout = (ConstraintLayout) findViewById(R.id.ConstraintLayout);
+        context = MainActivity.this;
+        bgButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        final CharSequence[] items = {getText(R.string.red), getText(R.string.yellow), getText(R.string.green), getText(R.string.purple)};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.message);
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                switch (item) {
+                    case 0:
+                        constraintLayout.setBackgroundResource(R.color.redColor);
+                        Toast.makeText(context, R.string.red, Toast.LENGTH_LONG).show();
+                        break;
+                    case 1:
+                        constraintLayout.setBackgroundResource(R.color.yellowColor);
+                        Toast.makeText(context, R.string.yellow, Toast.LENGTH_LONG).show();
+                        break;
+                    case 2:
+                        constraintLayout.setBackgroundResource(R.color.greenColor);
+                        Toast.makeText(context, R.string.green, Toast.LENGTH_LONG).show();
+                        break;
+                    case 3:
+                        constraintLayout.setBackgroundResource(R.color.purpleColor);
+                        Toast.makeText(context, R.string.purple, Toast.LENGTH_LONG).show();
+                        break;
+                }
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+}
